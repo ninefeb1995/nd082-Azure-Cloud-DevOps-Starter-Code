39cf639626b5b422f5a16cb8e834983bfd68844d
Thinh Le <ninefeb1995@gmail.com>
2022-04-25 01:10:05 +0700

    [ref] correct the order of var

--- C1 - Azure Infrastructure Operations/project/starter_files/variables.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/variables.tf
@@ -7,6 +7,12 @@ variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
 }
 
+variable "vm_names" {
+  description = "Name of the Virtual Machine"
+  type = list(string)
+  default = ["vm1", "vm2"]
+}
+
 variable "packer_image_name" {
   description = "Name of the Packer image"
   default     = "myPackerImage"
@@ -18,10 +24,4 @@ variable "username" {
 
 variable "password" {
   description = "Strong password"
-}
-
-variable "vm_names" {
-  description = "Name of the Virtual Machine"
-  type = list(string)
-  default = ["vm1", "vm2"]
 }
